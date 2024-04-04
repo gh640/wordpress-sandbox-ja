628793010e42563017d4fd569482215e6a62b33d
Goto Hayato <gh640@hey.com>
2024-04-04 00:24:39 +0000

    #18: Fix 443 redirection on GitHub Workspaces

--- codespaces/install-wordpress-on-docker.sh
+++ codespaces/install-wordpress-on-docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
-WP_URL="https://${CODESPACE_NAME}-80.githubpreview.dev"
+WP_URL="https://${CODESPACE_NAME}-80.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 WP_ADMIN_USER="admin"
 WP_ADMIN_EMAIL="example@example.com"
 WP_ADMIN_PASSWORD="password"
 
-exec docker compose exec wordpress \
-  wp --allow-root core install \
+docker compose exec -u www-data wordpress \
+  wp core install \
   --url="$WP_URL" \
   --title="WordPress サンドボックス" \
   --admin_user="$WP_ADMIN_USER" \
